cffac44662ad11565909a14996a244643480356c
Noeliamll76 <135754377+Noeliamll@users.noreply.github.com>
2023-11-10 18:23:49 +0100

    feat: conected DataBase in logUser. Token by console

--- src/common/LinkButton/LinkButton.jsx
+++ src/common/LinkButton/LinkButton.jsx
@@ -3,11 +3,10 @@ import './LinkButton.css'
 
 import { useNavigate } from 'react-router-dom'
 
-export const LinkButton = ({path, title}) =>{
-const navegate = useNavigate()
-
-return (
-    <div className="linkButtonDesign" onClick={()=>navegate(path)}>{title}
-    </div>
-)
+export const LinkButton = ({ path, title }) => {
+    const navegate = useNavigate()
+    return (
+        <div className="linkButtonDesign" onClick={() => navegate(path)}>{title}
+        </div>
+    )
 }
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,8 +1,74 @@
 
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import './Register.css'
+import { CustomInput } from "../../common/CustomInput/CustomInput";
+
+
 export const Register = () => {
+
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+        password: "",
+        phone: "",
+      });
+
+      const [userError, setUserError] = useState({
+        nameError: "",
+        emailError: "",
+        passwordError: "",
+        phoneError: "",
+      });
+
+    const functionHandler = (e) => {
+        setUser((prevState) => ({
+          ...prevState,
+          [e.target.name]: e.target.value
+        }));
+      };
+
+      const errorCheck = (e) => {
+        let error=""
+        error=validator(e.target.name, e.target.value);
+      }
+      
+
+
      return (
-         <div className="registerDesign">REGISTER</div>
+         <div className="registerDesign">       
+            <CustomInput
+                design={"inputDesign"}
+                type={"email"}
+                name={"email"}
+                placeholder={""}
+                functionProp={functionHandler}
+                functionBlur={errorCheck}
+                />
+                <div>{userError.emailError}</div>
+            <CustomInput
+                design={"inputDesign"}
+                type={"password"}
+                name={"password"}
+                placeholder={""}
+                functionProp={functionHandler}
+                functionBlur={errorCheck}
+                />
+                <CustomInput
+                design={"inputDesign"}
+                type={"text"}
+                name={"name"}
+                placeholder={""}
+                functionProp={functionHandler}
+                functionBlur={errorCheck}
+                />
+                <CustomInput
+                design={"inputDesign"}
+                type={"text"}
+                name={"phone"}
+                placeholder={""}
+                functionProp={functionHandler}
+                functionBlur={errorCheck}
+                />
+         </div>
      )
-}
+     }
