4967e4ff80cd8ab876325fc1b9f78963ad97d79c
Noeliamll76 <135754377+Noeliamll@users.noreply.github.com>
2023-11-11 20:32:18 +0100

    feat: Register user

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -4,17 +4,23 @@ import './Login.css'
 import { CustomInput } from "../../common/CustomInput/CustomInput";
 import { logUser } from "../../services/apiCalls";
 import { useNavigate } from 'react-router-dom';
+import { validator } from "../../services/useful";
 
 
 export const Login = () => {
 
     const navigate = useNavigate();
-
+    const [msgError, setMsgError] = useState('');
 
     const [credenciales, setCredenciales] = useState({
         email: "",
         password: "",
     });
+
+    const [credencialesError, setCredencialesError] = useState({
+        emailError: '',
+        passwordError: '',
+    })
 
     const functionHandler = (e) => {
         setCredenciales((prevState) => ({
@@ -22,20 +28,43 @@ export const Login = () => {
             [e.target.name]: e.target.value
         }));
     };
-    //   useEffect(()=>{
-    //     console.log(credenciales);
-    //   },[credenciales]);
+
+    const errorCheck = (e) => {
+        let error = "";
+        error = validator(e.target.name, e.target.value);
+
+        setCredencialesError((prevState) => ({
+            ...prevState,
+            [e.target.name + 'Error']: error,
+        }));
+    }
+
+    useEffect(() => {
+        console.log(credenciales);
+    }, [credenciales]);
 
     const logMe = () => {
-
+        for (let test1 in credenciales) {
+            if (credenciales[test1] === "") {
+                return;
+            }
+        }
+        for (let test in credencialesError) {
+            if (credencialesError[test] !== "") {
+                return;
+            }
+        }
         logUser(credenciales)
             .then(resultado => {
                 localStorage.setItem("token", (resultado.data.token))
                 setTimeout(() => {
                     navigate("/");
                 }, 1000);
-            } )
-            .catch(error => console.log(error));
+            })
+            .catch(error => {
+                console.log(error);
+                setMsgError(error.message)
+            });
     }
 
     return (
@@ -46,6 +75,7 @@ export const Login = () => {
                 name={"email"}
                 placeholder={""}
                 functionProp={functionHandler}
+                functionBlur={errorCheck}
             />
             <CustomInput
                 design={"inputDesign"}
@@ -53,10 +83,10 @@ export const Login = () => {
                 name={"password"}
                 placeholder={""}
                 functionProp={functionHandler}
+                functionBlur={errorCheck}
             />
             <div className='buttonSubmit' onClick={logMe}>Log Me!</div>
-
+            <div>{msgError}</div>
         </div>
-
     )
 }
