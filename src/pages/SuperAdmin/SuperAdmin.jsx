e744ca6f34852580027a14aac62491834db321bb
Noeliamll76 <135754377+Noeliamll@users.noreply.github.com>
2023-11-23 17:23:31 +0100

    Feat: I add ternary to control inappropriate access to SuperAdmin view, if the role is not super-admin

--- src/pages/SuperAdmin/SuperAdmin.jsx
+++ src/pages/SuperAdmin/SuperAdmin.jsx
@@ -1,40 +1,44 @@
 import './SuperAdmin.css'
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useSelector } from "react-redux";
+import { userData } from "../../pages/userSlice";
+import { jwtDecode } from 'jwt-decode';
 
 export const SuperAdmin = () => {
 
     const navigate = useNavigate();
-    
-   
+    const rdxUser = useSelector(userData);
+    const tokenDecodificated = jwtDecode(rdxUser.credentials.token);
+    const role = tokenDecodificated.role
 
-    const callUsersListClick = () => {
-        
-        navigate("/userList");
-    }
-    const callUsersAppointmentsClick = () => {
-       
-        navigate("/userAppointments");
-    }
-    const callCreateArtistClick = () => {
-        
-        ("/createArtist");
-    }
+    const callUsersListClick = () => { navigate("/userList"); }
+    const callUsersAppointmentsClick = () => { navigate("/userAppointments"); }
+    const callCreateArtistClick = () => { navigate("/createArtist"); }
+    
     return (
         <>
             <div className="superDesign">
-                <div className={`superSubmit `} onClick={callUsersListClick}>
-                Show user list
-                </div>
-        
-                <div className={`superSubmit `} onClick={callUsersAppointmentsClick}>
-                Show user appointments
-                </div>
-          
-                <div className={`superSubmit `} onClick={callCreateArtistClick}>
-                Create tattoo artist
-                </div>
+                {role === "super_admin"
+                    ? (
+                        <>
+                            <div className={`superSubmit `} onClick={callUsersListClick}>
+                                Show user list
+                            </div>
 
+                            <div className={`superSubmit `} onClick={callUsersAppointmentsClick}>
+                                Show user appointments
+                            </div>
+
+                            <div className={`superSubmit `} onClick={callCreateArtistClick}>
+                                Create tattoo artist
+                            </div>
+                        </>
+                    )
+                    : (
+                        navigate("/")
+                    )
+                }
             </div>
         </>
     )
